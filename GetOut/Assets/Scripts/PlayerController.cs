76f70ea564cafcbade30c2b5f5d4069d94186325
ArthurRaoult <art.raoult@gmail.com>
2022-06-28 14:54:04 -0400

    Tuesday Afternoon 2:54

--- GetOut/Assets/Scripts/PlayerController.cs
+++ GetOut/Assets/Scripts/PlayerController.cs
@@ -6,17 +6,25 @@ public class PlayerController : MonoBehaviour
 {
     public float playerSpeed = 100f;
     public float jumpHeight = 5f;
-
     private Rigidbody2D playerRb;
     public bool isOnGround = true;
     private BoxCollider2D PlayerBox;
+
+    //invisibility
     public bool Invis = false;
     private float InvisStart = 0f;
     private float InvisCooldown = 8f;
     private float InvisTime = 2f;
+    //color
     private SpriteRenderer PlayerState;
     public float translu = 0.25f;
     public Color Playercolorchanger = Color.white;
+    //trash
+    public bool hastrash = false;
+    public GameObject trashcan;
+    public GameObject player;
+    public BoxCollider2D TrashCollision;
+    public float Difficultyinteger;
 
     // Start is called before the first frame update
     void Start()
@@ -24,7 +32,8 @@ public class PlayerController : MonoBehaviour
         playerRb = GetComponent<Rigidbody2D>();
         PlayerBox = GetComponent<BoxCollider2D>();
         PlayerState = GetComponent<SpriteRenderer>();
-
+        TrashCollision = GetComponentInChildren<BoxCollider2D>();
+        trashcan.SetActive(false);
     }
 
     // Update is called once per frame
@@ -34,6 +43,7 @@ public class PlayerController : MonoBehaviour
         {
             float horizontalInput = Input.GetAxis("Horizontal");
             transform.Translate(Vector2.right  * playerSpeed * horizontalInput / 10);
+            //Look left and right
             if (horizontalInput < 0) {
                 PlayerState.flipX = true;
             } else if(horizontalInput > 0)
@@ -41,7 +51,7 @@ public class PlayerController : MonoBehaviour
                 PlayerState.flipX = false;
             }
         }
-        //Look left and right
+        
 
         //Crouching
         {
@@ -85,14 +95,31 @@ public class PlayerController : MonoBehaviour
             PlayerState.material.color = new Color(1f, 1f, 1f, 1f);
 
         }
+        //Trash Movement
+        trashcan.transform.position = player.transform.position + new Vector3(-0.6f, 1.8f, 0);
+        //Trash Throw
+
     }
 
     //Jump on floor
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground"))
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Walltypeobj"))
         {
             isOnGround = true;
         }
     }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if ((collision.gameObject.tag == "Trashcan") && Input.GetKeyDown(KeyCode.E) && !hastrash)
+        {
+            Debug.Log("Trash enabled");
+            hastrash = true;
+            trashcan.SetActive(true);
+        }
+    }
+   
+
+
 }
